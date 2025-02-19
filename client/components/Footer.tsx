f8d809cb22b3e9b63be846284caac96545ddb2e4
phan van huan <pvhuan552000@gamil.com>
2025-02-19 23:30:58 +0700

    upd footer client

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
     return (
-        <div className="text-center text-heading2-bold py-6">Footer</div>
+        <div className="text-center text-heading2-bold py-6">Footer shop</div>
     )
 }
 

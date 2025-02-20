dbce13f8db64af20d7312bfea11d86e722cb2278
phan van huan <pvhuan552000@gmail.com>
2025-02-20 19:07:17 +0700

    config(admin):add SKU to product schema

--- admin/lib/models/Product.ts
+++ admin/lib/models/Product.ts
@@ -5,6 +5,10 @@ const ProductSchema = new mongoose.Schema({
         type: String,
         require: true,
     },
+    sku:{
+        type: String,
+        require: true,
+    },
     description: {
         type: String,
     },

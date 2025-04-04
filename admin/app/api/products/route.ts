4a6dd3c063263aaa5c5f4cd148024e48449553a3
phan van huan <pvhuan552000@gmail.com>
2025-04-04 17:00:34 +0700

    config: rewrite GET to fit request

--- admin/app/api/products/route.ts
+++ admin/app/api/products/route.ts
@@ -2,6 +2,7 @@ import Collection from "@/lib/models/Collection";
 import Product from "@/lib/models/Product";
 import { connectToDB } from "@/lib/mongoDB";
 import { auth } from "@clerk/nextjs/server";
+import mongoose from "mongoose";
 import { NextRequest, NextResponse } from "next/server";
 
 
@@ -73,7 +74,36 @@ import { NextRequest, NextResponse } from "next/server";
 export const GET = async (req: NextRequest) => {
     try {
         await connectToDB();
-        const products = await Product.find().sort({ createdAt: 'desc' }).populate({ path: "collections", model: Collection });
+        const {searchParams} = new URL(req.url);
+        // get the params from the request
+        const productParams = searchParams.get("data");
+        // get all products
+        if(!productParams) {
+            const allProducts = await Product.find().sort({ createdAt: 'desc' }).populate({ path: "collections", model: Collection });
+            return NextResponse.json(allProducts, { status: 200 });
+        }
+        // get all products with ids
+        if(productParams==="ids") {
+            const productIds = await Product.find({}, "_id").sort({ createdAt: 'desc' });
+            return NextResponse.json(productIds, { status: 200 });
+        }
+        // get all products with ids and titles
+        if(productParams === "idsAndTitles"){
+            const productIdsAndTitles = await Product.find({}, "_id title").sort({ createdAt: 'desc' });
+            return NextResponse.json(productIdsAndTitles, { status: 200 });
+        }
+        // get all products with array of ids
+        // check if productParams is a string of ids separated by commas
+        const productIds = productParams.includes(",") ? productParams.split(",") : [productParams];
+        const validProductIds = productIds.filter((id:string) => mongoose.isValidObjectId(id));
+        //check if all productIds are valid
+        if(validProductIds.length !== productIds.length || validProductIds.length === 0) {
+            return NextResponse.json({error: "Invalid product ID(s)"}, { status: 400 });
+        }
+        const products = await Product.find({ _id: { $in: validProductIds } }).sort({ createdAt: 'desc' }).populate({ path: "collections", model: Collection });
+        if(products.length === 0) {
+            return NextResponse.json({error: "No products found"}, { status: 404 });
+        }
         return NextResponse.json(products, { status: 200 });
     } catch (error) {
         console.log("Products_GET", error);
